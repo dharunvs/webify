1e13a80307cd3a3c06ea490fefdda4790b4723f0
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-08-28 17:52:42 +0530

    Updates

--- app/src/components/PageComponents/Menu.jsx
+++ app/src/components/PageComponents/Menu.jsx
@@ -1,7 +1,10 @@
 function Menu() {
   return (
     <section className="Menu" name="menu">
-      Menu
+      <div className="menuHeader">
+        <p>Specialities</p>
+        <h1>Our Menu</h1>
+      </div>
     </section>
   );
 }
--- app/src/components/PageComponents/SpecialDishes.jsx
+++ app/src/components/PageComponents/SpecialDishes.jsx
@@ -1,7 +1,57 @@
+import pizza from "../../assets/images/pizza.jpg";
+import beef from "../../assets/images/beef.jpg";
+
 function SpecialDishes() {
   return (
     <section className="SpecialDishes" name="specialDishes">
-      SpecialDishes
+      <div className="specialDishesHeader">
+        <p>Specialities</p>
+        <h1>Special Dishes</h1>
+      </div>
+      <div className="SpecialDishesGallery">
+        <div className="gallery">
+          <div className="sd1">
+            <h1>01.</h1>
+          </div>
+          <div className="sd2">
+            <h1>Pepper Soup</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              nihil quis ipsam neque commodi, aspernatur quod pariatur harum
+              veritatis distinctio libero soluta explicabo blanditiis minus
+              corporis dicta sit quidem? Nam.
+            </p>
+            <h2>Rs 649.00</h2>
+            <button className="defaultButton">Book A Table</button>
+          </div>
+          <div className="sd3 sd31">
+            <div className="imgContainer">
+              <img src={pizza} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="gallery">
+          <div className="sd3 sd32">
+            <div className="imgContainer">
+              <img src={beef} alt="" className="customimg43" />
+            </div>
+          </div>
+          <div className="sd1">
+            <h1>02.</h1>
+          </div>
+          <div className="sd2">
+            <h1>Pepper Soup</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              nihil quis ipsam neque commodi, aspernatur quod pariatur harum
+              veritatis distinctio libero soluta explicabo blanditiis minus
+              corporis dicta sit quidem? Nam.
+            </p>
+            <h2>Rs 649.00</h2>
+            <button className="defaultButton">Book A Table</button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
