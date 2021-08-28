da53eda620bba2c99ba836fa45b68fa0e6aaee88
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-08-28 16:37:03 +0530

    Update About.jsx

--- app/src/components/PageComponents/About.jsx
+++ app/src/components/PageComponents/About.jsx
@@ -1,7 +1,12 @@
 function About() {
   return (
     <section className="About" name="about">
-      About
+      <div className="aboutLeft"></div>
+      <div className="aboutRight">
+        <p>About</p>
+        <h1>Welcome to KCG Restaurant</h1>
+        <p>{/* Passage here */}</p>
+      </div>
     </section>
   );
 }
