911f29bf132cffca1f560d05bc36ab7a5f96890f
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-08-28 16:42:50 +0530

    Updates

--- app/src/components/PageComponents/About.jsx
+++ app/src/components/PageComponents/About.jsx
@@ -1,11 +1,37 @@
 function About() {
   return (
     <section className="About" name="about">
-      <div className="aboutLeft"></div>
+      <div className="aboutLeft">
+        <div className="imgContainer">
+          <img src="" alt="" />
+        </div>
+      </div>
       <div className="aboutRight">
         <p>About</p>
         <h1>Welcome to KCG Restaurant</h1>
         <p>{/* Passage here */}</p>
+
+        <h2>Special Recipe</h2>
+        <div className="aboutMiniCards">
+          <div>
+            <div className="imgContainer">
+              <img src="" alt="" />
+            </div>
+            <p>Pepper Soup</p>
+          </div>
+          <div>
+            <div className="imgContainer">
+              <img src="" alt="" />
+            </div>
+            <p>Pepper Soup</p>
+          </div>
+          <div>
+            <div className="imgContainer">
+              <img src="" alt="" />
+            </div>
+            <p>Pepper Soup</p>
+          </div>
+        </div>
       </div>
     </section>
   );
